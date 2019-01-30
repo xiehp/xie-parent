6b14472b52e2be31759235e510afe8601f2ae881
xiehp <94540413@qq.com>
2019-01-31 01:13:20 +0800

    update

--- script/package/build clean install product-timer .bat
+++ script/package/build clean install product-timer .bat
@@ -7,8 +7,9 @@ cd ..
 echo  %cd%
 
 echo  开始build
-mvn -PproductRemote,timer -B -U -e  -Dmaven.test.skip=true -DskipTests --threads 2 -s D:\work\maven\maven-setting.xml clean install
+mvn -Pproduct,timer -B -U -e  -Dmaven.test.skip=true -DskipTests --threads 2 -s D:\work\maven\maven-setting.xml clean install
 
 echo 回到原来cd所在目录
 cd %nowPath%
+
 pause
